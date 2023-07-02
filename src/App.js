ab5dc3a33b1a4f789193a536dff9d536cd49da33
krishalaani <127039265+krishalaani@users.noreply.github.com>
2023-07-02 20:08:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>Your To-Do List </h1>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
